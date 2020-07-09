00e3d308d1dec936b5836bea6e33281c23220d86
Yogesh <yogeshpatel984.yp@gmail.com>
2020-07-09 11:19:46 +0530

    Dne

---
+++ StaticTableview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StaticTableview
+//
+//  Created by Yogesh Patel on 26/12/19.
+//  Copyright © 2019 yogesh patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
